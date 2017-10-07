35a4edd68389f7ac77279f7a4caaaa597604a3ba
Jan Riethmayer <jan@riethmayer.de>
2017-10-07 15:42:22 +0100

    Add solving tiles

--- src/GameRunning/GameBoard/GameBoard.js
+++ src/GameRunning/GameBoard/GameBoard.js
@@ -4,13 +4,15 @@ import Tile from '../../components/Tile/Tile'
 import { clicked } from '../../reducers/gameBoard.js'
 
 class GameBoard extends Component {
-  tile(name, index) {
+  tile(tileData) {
+    const { token, index, active } = tileData
     const { clicked } = this.props
     return(
-      <Tile caption={name}
+      <Tile caption={ token }
+            active={ active }
             id={`tile_${index}`}
-            onClick={ () => clicked(name, index) }
-            key={`tile_${index}`}/>
+            onClick={ () => clicked(index) }
+            key={ index } />
     )
   }
 
@@ -18,8 +20,8 @@ class GameBoard extends Component {
     const { board } = this.props
     return (
       <div>
-        { Object.keys(board).map(t => this.tile(board[t], t)) }
         <h1>Game Board </h1>
+        { Object.keys(board).map(t => this.tile(board[t])) }
       </div>
     )
   }
--- src/reducers/gameBoard.js
+++ src/reducers/gameBoard.js
@@ -1,8 +1,9 @@
+import _ from 'lodash'
+
 const numbers_0_to_9 = [...Array(10)].map((_, i) => String(i))
 const alphabet = [...Array(26)].map((_, i) => String.fromCharCode(i+65))
 const tokens = numbers_0_to_9.concat(alphabet)
 const allTokens = tokens.concat(tokens).concat(tokens).concat(tokens)
-
 const shuffle = (array) => {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -11,73 +12,101 @@ const shuffle = (array) => {
   return array;
 }
 
-export const clicked = (name, index) => {
+export const clicked = (clickedIndex) => {
   return (dispatch, state) => {
     const { board } = state()
-    if(board['name'] == name) {
-      if(board['index'] == index) {
-        dispatch(deselected({name, index}))
-      }
+    const { index, token, active } = board[clickedIndex]
+    console.log(token, index, active)
+    if(active) {
+      dispatch(deselected(index))
+    } else {
+      dispatch(selected(index))
+      dispatch(solve(index))
     }
-    console.log('clicked', board)
-    // 0. nothing is selected:
-    // 1. click tile with caption 'A' and id=5
-    dispatch(selected({name, index}))
-    // 1.1 tile with id 5 becomes active
-    // 1.2 tiles with caption 'A' become solvable  // maybe?
-    // 1.3.1 click elem with id=5 again
-    // 1.3.1.1 tile with id 5 becomes inactive
-    // 1.3.1.2 tiles with caption 'A' lose solvable
-    // 1.4.1 click elem with caption 'B' and id=2
-    // 1.4.1.1 tile with id=2 and caption 'B' becomes active
-    // 1.4.1.2 tiles with caption 'B' become solvable...
-    // 1.4.1.3 tile with caption 'B' and id=38 clicked
-    // 1.4.1.3.1 remove tile 38 and 2 from the board
-    // 1.4.1.3.2 tiles lose solvable
   }
 }
 
-const selected = ({name, index}) => {
+const selected = (index) => {
   return (dispatch) => {
-    console.log('actionCreator', name, index)
+    console.log('actionCreator', index)
     dispatch({
       type: actions.selected,
-      name,
       index
     })
   }
 }
 
-const deselected = ({name, index}) => {
+const deselected = (index) => {
   return (dispatch) => {
-    console.log('deselected', name, index)
+    console.log('deselected', index)
     dispatch({
       type: actions.deselected,
-      name,
       index
     })
   }
 }
 
+const solve = (currentIndex) => {
+  return (dispatch, state) => {
+    const { board } = state()
+    const { token } = board[currentIndex]
+    const solved = Object.keys(board).filter((id) => {
+      let tile = board[id]
+      return tile &&
+             (tile['token'] === token) &&
+             tile['active'] &&
+             (tile['index'] !== currentIndex)
+    })
+    console.log('length', solved.length, solved)
+    if(solved.length > 0) {
+      dispatch({
+        type: actions.solved,
+        index: currentIndex,
+        solved: solved
+      })
+    }
+  }
+}
+
+
 const actions = {
-  new: 'NEW',
   selected: 'SELECTED',
-  deselected: 'DESELECTED'
+  deselected: 'DESELECTED',
+  solved: 'SOLVED'
 }
 
 const initialState = shuffle(allTokens)
   .map((token, index) => { return [token, index] }) //
-  .reduce((obj, [token, index]) => { obj[index]= token; return(obj) }, {})
+  .reduce((obj, [token, index]) => {
+    obj[index]= {
+      index: index,
+      token: token,
+      active: false
+    }
+    return(obj)
+  }, {})
 
 const gameBoard = (state = initialState, action = {}) => {
+  const tile = state[action.index]
   switch(action.type) {
-    case actions.new:
-      return initialState
     case actions.selected:
-      console.log('reducer', action.name, action.index)
-      return { ...state, name: action.name, index: action.index }
+      return {
+        ...state,
+        [action.index]: {
+          ...tile,
+          active: true
+        }
+      }
     case actions.deselected:
-      return { ...state, name: undefined, index: undefined }
+      return {
+        ...state,
+        [action.index]: {
+          ...tile,
+          active: false
+        }
+      }
+    case actions.solved:
+      return _.omit(state, action.index, ...action.solved)
     default:
       return state
   }
