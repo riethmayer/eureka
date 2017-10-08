3b6fd58b2fa855f7fa8382e5b30aefb88e09251d
Jan Riethmayer <jan@riethmayer.de>
2017-10-08 13:11:10 +0100

    Make elems clickable
    * problem is things don't stay in place after clicking

--- src/reducers/gameBoard.js
+++ src/reducers/gameBoard.js
@@ -89,16 +89,87 @@ const actions = {
   cleanup: 'CLEANUP'
 }
 
+const setupLayer = (index) => {
+  switch(true) {
+    case (index >= 87 && index <= 122):
+      return 1
+    case (index >= 123 && index <= 138):
+      return 2
+    case (index >= 139 && index <= 142):
+      return 3
+    case (index === 143):
+      return 4
+    default:
+      return 0
+  }
+}
+
+const setupRow = (index) => {
+  switch(true) {
+    case (index <= 11):
+      return 1
+    case (index >= 12 && index <= 19):
+      return 2
+    case (index >= 20 && index <= 29):
+      return 3
+    case (index >= 30 && index <= 41):
+      return 4
+    case (index === 42):
+      return 5 // single left stone
+    case (index >= 43 && index <= 54):
+      return 6
+    case (index >= 55 && index <= 56):
+      return 7 // right two stones
+    case (index >= 57 && index <= 66):
+      return 8
+    case (index >= 67 && index <= 74):
+      return 9
+    case (index >= 75 && index <= 86):
+      return 10
+    case (index >= 87 && index <= 92):
+      return 11
+    case (index >= 93 && index <= 98):
+      return 12
+    case (index >= 99 && index <= 104):
+      return 13
+    case (index >= 105 && index <= 110):
+      return 14
+    case (index >= 111 && index <= 116):
+      return 15
+    case (index >= 117 && index <= 122):
+      return 16
+    case (index >= 123 && index <= 126):
+      return 17
+    case (index >= 127 && index <= 130):
+      return 18
+    case (index >= 131 && index <= 134):
+      return 19
+    case (index >= 135 && index <= 138):
+      return 20
+    case (index >= 139 && index <= 140):
+      return 21
+    case (index >= 141 && index <= 142):
+      return 22
+    case (index === 143):
+      return 23
+    default:
+      return 0
+  }
+}
+
 const initialState = shuffle(allTokens)
   .map((token, index) => { return [token, index] }) //
   .reduce((obj, [token, index]) => {
     obj[index]= {
       index: index,
       token: token,
-      active: false
+      active: false,
+      layer: setupLayer(index),
+      row: setupRow(index)
     }
     return(obj)
   }, {})
+
 
 const gameBoard = (state = initialState, action = {}) => {
   const tile = state[action.index]
