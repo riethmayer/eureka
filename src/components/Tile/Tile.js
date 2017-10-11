5b96da83e0cac388c7fdeba5481cf6bf0ae2195e
Jan Riethmayer <jan@riethmayer.de>
2017-10-12 00:21:02 +0100

    Refactor colors, fix wobbling effect by adding svg

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import _ from 'lodash'
+import color from './colors'
+import './tile.css'
 
 class Tile extends Component {
   constructor(props) {
@@ -9,80 +11,65 @@ class Tile extends Component {
     }
   }
 
-  color(key) {
-    const colors = {
-      a: 'blue',
-      b: 'red',
-      c: 'green',
-      d: 'brown',
-      e: 'purple',
-      f: 'orange',
-      g: 'yellow',
-      h: 'aqua',
-      i: 'blueviolet',
-      j: 'cadetblue',
-      k: 'coral',
-      l: 'cornflowerblue',
-      m: 'crimson',
-      n: 'cyan',
-      o: 'darkblue',
-      p: 'darkcyan',
-      q: 'darkgreen',
-      r: 'darksalmon',
-      s: 'deepskyblue',
-      t: 'deeppink',
-      u: 'yellow',
-      v: 'forestgreen',
-      w: 'greenyellow',
-      x: 'palevioletred',
-      y: 'darkseagreen',
-      z: 'tomato',
-      0: 'orchid',
-      1: 'peru',
-      2: 'steelblue',
-      3: 'thistle',
-      4: 'teal',
-      5: 'wheat',
-      6: 'turquoise',
-      7: 'springgreen',
-      8: 'papayawhip',
-      9: 'olivedrab'
-    }
-    return colors[String(key).toLowerCase()]
-  }
-
-  render() {
-    const { token,
-            active,
-            layer,
-            column,
-            row,
-            onClick } = this.props
-    let className = _.compact([
+  classes = ({token, layer, column, row, active}) => {
+    return _.compact([
       'tile',
-      `tile-${String(token).toLowerCase()}`,
+      'rect',
       `layer-${layer}`,
       `column-${column}`,
       `row-${row}`,
       `${active ? 'active' : ''}`
     ]).join(" ")
+  }
+
+  render() {
+    const { onClick, token } = this.props
     return (
-      <svg xmlns="http//www.w3.org/2000/svg"
-           fill="gray"
-           stroke="none"
-           height="36"
-           width="36"
-           onClick={onClick}
-           className={`${className} rect`}>
-        <text x="50%"
-              y="60%"
-              className='rect-text'
-              alignmentBaseline="middle"
-              textAnchor="middle"
-              fill={this.color(token) || 'white'}>
-          { token || 'nil' }
-        </text>
-      </svg>
+      <div onClick={onClick}
+           className={ this.classes(this.props) }>
+        <svg xmlns="http//www.w3.org/2000/svg"
+             fill="gray"
+             viewBox="0 0 370 373"
+             stroke="none"
+             height="370"
+             width="373">
+          <defs>
+            <linearGradient x1="0.25781251%"
+                            y1="49.7500002%"
+                            x2="101.257813%"
+                            y2="49.7500002%"
+                            id="linearGradient-1">
+              <stop stopColor="#cdcdcd" offset="0%"></stop>
+              <stop stopColor="#666666" offset="47.5247532%"></stop>
+              <stop stopColor="#666666" offset="100%"></stop>
+            </linearGradient>
+          </defs>
+          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="r-copy" transform="translate(-15.000000, -20.000000)">
+              <g id="Tile" transform="translate(15.000000, -54.000000)">
+                <g strokeWidth="1" transform="translate(0.000000, 74.000000)">
+                  <rect className="inner-rectangle"
+                        x="0"
+                        y="0"
+                        width="347"
+                        height="346"></rect>
+                  <text x="50%"
+                        y="80%"
+                        className='rect-text'
+                        alignmentBaseline="middle"
+                        textAnchor="middle" >
+                    <tspan className="inner-token" fill={color(token) || 'white'} >
+                      { token || 'nil' }
+                    </tspan>
+                    
+                  </text>
+                </g>
+                <path d="M369.703563,445.690524 L370,445.986636 L370,101 L346,74 L346,420 L1.1937118e-12,420 L26.8543537,446.001266 L369.993318,446.006682 L369.703563,445.690524 Z" fill="url(#linearGradient-1)"></path>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
     )
   }
 }
---
+++ src/components/Tile/colors.js
@@ -0,0 +1,44 @@
+export const colors = {
+  a: 'blue',
+  b: 'red',
+  c: 'green',
+  d: 'brown',
+  e: 'purple',
+  f: 'orange',
+  g: 'yellow',
+  h: 'aqua',
+  i: 'blueviolet',
+  j: 'cadetblue',
+  k: 'coral',
+  l: 'cornflowerblue',
+  m: 'crimson',
+  n: 'cyan',
+  o: 'darkblue',
+  p: 'darkcyan',
+  q: 'darkgreen',
+  r: 'darksalmon',
+  s: 'deepskyblue',
+  t: 'deeppink',
+  u: 'yellow',
+  v: 'forestgreen',
+  w: 'greenyellow',
+  x: 'palevioletred',
+  y: 'darkseagreen',
+  z: 'tomato',
+  0: 'orchid',
+  1: 'peru',
+  2: 'steelblue',
+  3: 'thistle',
+  4: 'teal',
+  5: 'wheat',
+  6: 'turquoise',
+  7: 'springgreen',
+  8: 'papayawhip',
+  9: 'olivedrab'
+}
+
+const color = (key) => {
+  return colors[String(key).toLowerCase()]
+}
+
+export default color
