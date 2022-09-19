eaf282443feed6c9caed84f7a53132813ddf8a85
Jan Riethmayer <jan@riethmayer.de>
2022-09-19 20:29:39 +0100

    Ensure full page height on html and body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="bg-black">
+    <Html lang="en" className="bg-black h-full">
       <Head />
-      <body>
+      <body className="h-full">
         <Main />
         <NextScript />
       </body>
