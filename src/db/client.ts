a478ce13c0b003aae7aaa8bfa1d4c56fa2c53bff
Jan Riethmayer <jan@riethmayer.de>
2024-10-10 15:32:28 +0100

    set url again

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,7 +3,7 @@ import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 
 export const client = createClient({
-  url: process.env.TURSO_DATABASE_URL || "",
+  url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN || "",
 });
 
